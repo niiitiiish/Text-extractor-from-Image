a2d19a40b973d257663f1ba7c1b678c3dacfa327
Nitish kumar <144931952+niiitiiish@users.noreply.github.com>
2024-12-15 18:50:01 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,4 +1,3 @@
-#jai jagannath
 import easyocr 
 import streamlit as st
 import torch
@@ -17,17 +16,17 @@ if st.button("EXTRACT"):
     if uploaded_file:
         image=Image.open(uploaded_file)
         st.image(image,caption="uploaded image",use_container_width =True)
-         with st.spinner("in Progress"):
-                image_np=np.array(image)
-                result=reader.readtext(image_np)
-                extracted_text=[]
-                st.write("Extracted text: ")
-                for(_,text,_)in result:
-                    extracted_text.append(text)
-                    
-                    
-                textract= " ".join(extracted_text)
-                st.text_area("Extracted Text:", textract, height=300)
+        with  st.spinner("in Progress"):
+       
+            image_np=np.array(image)
+            result=reader.readtext(image_np)
+            extracted_text=[]
+            st.write("Extracted text: ")
+            for(_,text,_)in result:
+                extracted_text.append(text)
+
+            textract= " ".join(extracted_text)
+            st.text_area("Extracted Text:", textract, height=300)
       
     else:
         st.error("please upload the corrrect image file")
