f89f3250601afd88f68a3f0ba9f5ff4edb5fc4a3
Nitish kumar <144931952+niiitiiish@users.noreply.github.com>
2024-12-23 13:22:06 +0530

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -5,7 +5,7 @@ import numpy as np
 from PIL import Image
 
 
-reader=easyocr.Reader(['en'])
+reader=easyocr.Reader(['en'],modal_storage_directory='.')
 st.title("Image Text extractor")
 st.write("upload an image:")
 
